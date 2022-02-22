97d7974fe05f1e30d6b2cb49760cbbdcaa13c4f6
TC <teresa.j.campbell@gmail.com>
2022-02-21 19:08:27 -0500

    added associations

--- models/index.js
+++ models/index.js
@@ -6,11 +6,26 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.hasOne(Category, {
+  foreignKey: 'category_name'
+});
+
+
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_name'
+
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag, {
+  foreignKey: 'tag_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  foreignKey: 'product_id'
+});
 
 module.exports = {
   Product,
